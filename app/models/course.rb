e39cd22cbbfb407826fb4578d2a3ed3600ab36aa
Vivekanand Rao <vivek.rao@teachscape.com>
2015-12-13 18:13:49 -0800

    Added presence Validation.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ActiveRecord::Base
   has_many :topics
+  validates_presence_of :name, :description, :author, :duration, :level, :rating, :released
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
   belongs_to :course
+  validates_presence_of :title, :topicduration
 end
